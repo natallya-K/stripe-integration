b3a73b11cbbbbabe30544a42177f31cfe06a2b71
natallya-K <kavalkova@yahoo.fr>
2024-07-04 18:02:50 +0200

    working version

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 require('dotenv').config()
+const cors = require('cors');
 const express = require('express')
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 const { getProducts, createOrder } = require('./printful');
-const pool = require('./db');
+//const pool = require('./db');
 
 const app = express()
 const port = process.env.PORT || 3000;
 
+app.use(cors());
+//app.use(cors({
+//    origin: process.env.FRONTEND_URL
+// }));
 app.use(express.json());
 app.set('view engine', 'ejs')
 
@@ -16,33 +21,36 @@ app.get('/', (req, res) => {
 })
 
 app.post('/checkout', async (req, res) => {
-    const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items: [
-            {
-                price_data: {
-                    currency: 'usd',
-                    product_data: {
-                        name: 'Canvas Print',
+    try {
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            line_items: [
+                {
+                    price_data: {
+                        currency: 'usd',
+                        product_data: {
+                            name: 'Canvas Print',
+                        },
+                        unit_amount: 100 * 100,
                     },
-                    unit_amount: 100 * 100,
+                    quantity: 1,
                 },
-                quantity: 1,
+            ],
+            mode: 'payment',
+            shipping_address_collection: {
+                allowed_countries: ['US', 'CA', 'GB', 'BE', 'AU', 'DE', 'FR', 'IT', 'JP', 'NL', 'NZ', 'ES', 'CH', 'AT', 'DK', 'NO', 'SE', 'SG'],
             },
-        ],
-        mode: 'payment',
-        shipping_address_collection: {
-        allowed_countries: ['US', 'CA', 'GB', 'BE', 'AU', 'DE', 'FR', 'IT', 'JP', 'NL', 'NZ', 'ES', 'CH', 'AT', 'DK', 'NO', 'SE', 'SG'],
-        },
-        success_url: `${process.env.BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${process.env.BASE_URL}/cancel`,
-        // Keep this cancel route is case of need to keep info on cancel
-    })
-    // res.json({ id: session.id })
-    // console.log(session)
-    res.redirect(session.url)
+            success_url: `${process.env.BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+            cancel_url: `${process.env.BASE_URL}/cancel`,
+        });
 
-})
+        res.json({ url: session.url }); // Return the session URL in the JSON response
+    } catch (error) {
+        console.error("Error creating checkout session", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
 
 app.get('/success', async (req, res) => {
     // console.log(req.query.session_id)
